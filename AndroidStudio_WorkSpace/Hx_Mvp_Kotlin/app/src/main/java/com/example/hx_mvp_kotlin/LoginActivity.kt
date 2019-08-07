98074806f1790432fd7ce23dda4f23fab96f3169
winter0503 <915540909@qq.com>
2019-08-07 14:36:47 +0800

    动态权限的申请

--- AndroidStudio_WorkSpace/Hx_Mvp_Kotlin/app/src/main/java/com/example/hx_mvp_kotlin/LoginActivity.kt
+++ AndroidStudio_WorkSpace/Hx_Mvp_Kotlin/app/src/main/java/com/example/hx_mvp_kotlin/LoginActivity.kt
@@ -1,11 +1,15 @@
 package com.example.hx_mvp_kotlin
 
+import android.content.pm.PackageManager
+import android.support.v4.app.ActivityCompat
 import android.widget.Toast
 import com.example.hx_mvp_kotlin.contract.LoginContract
 import com.example.hx_mvp_kotlin.presenter.LoginPresenter
 import kotlinx.android.synthetic.main.activity_login.*
 import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.toast
+import java.util.jar.Manifest
+
 
 /**
  * Create by 心跳 on 2019/8/3 10:42
@@ -27,9 +31,25 @@ class LoginActivity:BaseActivity(),LoginContract.View{
     fun login(){
         //隐藏软键盘
         hideSoftKeyboar()
-        val name = userName.text.trim().toString()
-        val pwd =password.text.trim().toString()
-        persenter.login(name,pwd)
+
+        //检查是否有读写Sdcard 的权限
+        if(checkPermission()){
+            val name = userName.text.trim().toString()
+            val pwd =password.text.trim().toString()
+            persenter.login(name,pwd)
+        }else  registePermission()
+
+    }
+
+    private fun registePermission() {
+        val premissions = arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        ActivityCompat.requestPermissions(this,premissions,0)
+    }
+
+    //检查权限
+    private fun checkPermission(): Boolean{
+        val result = ActivityCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        return result==PackageManager.PERMISSION_GRANTED
     }
     override fun getLayoutResId(): Int =R.layout.activity_login
 
@@ -66,5 +86,14 @@ class LoginActivity:BaseActivity(),LoginContract.View{
 
         //提示用户
         toast(R.string.login_failed)
+    }
+
+    //用户选择是否开启权限的回调方法
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+            //用户开启了需要的权限，开始登录验证
+            login()
+        }else toast(R.string.permission_denied)
     }
 }
